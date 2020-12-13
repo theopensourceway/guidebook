2724e71ee6a28d895821059063cd1fc558dac1e6
quaid <quaid@iquaid.org>
2020-12-13 15:18:25 -0800

    2.0 test build - working! HTML PDF tools updated - Updating tools to build the 2.0 - Adjustments to all section files with	includes to get	build working - HTML and PDF build without error - 147 pages PDF, 65K words, 12 writers,	20 chapters - This represents the first look at the	final release, in terms	of commenting out or removing or renaming or whatev er was needed based on recent conversations, decisions across other issues and commits,	and so forth. - At this moment, this build represents the collective wisdom as we planned it last Thursday's meeting and in online discussions since (in git repo comments). - This render is now live and will be maintained (for now) at: -- https://www.theopensourceway.org/the_open_source_way-guidebook-2.0.html -- https://www.theopensourceway.org/the_open_source_way-guidebook-2.0.pdf

--- build-book.sh
+++ build-book.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 echo "building html5 ..."
-asciidoctor -d book preview-1.99-book.adoc
+asciidoctor -d book the_open_source_way-guidebook-2.0.adoc
 echo "building pdf ..."
-asciidoctor-pdf -d book  preview-1.99-book.adoc
+asciidoctor-pdf -d book the_open_source_way-guidebook-2.0.adoc
 echo "done!"
--- publish-book.sh
+++ publish-book.sh
@@ -13,15 +13,15 @@ echo "running book build script ..."
 ./build-book.sh
 # copy the artifacts into the website directory in a relative location
 echo "copying HTML and PDF output to website repo ..."
-cp preview-1.99-book.{pdf,html} ../theopensourceway_old_website/website/
+cp the_open_source_way-guidebook-2.0.{pdf,html} ../theopensourceway_old_website/website/
 # switch over the the website directory for the rest of the script
 echo "committing and pushing new build to the website ..."
 cd ../theopensourceway_old_website/website/
 # check the artificats into the git repo 
 echo "git add ..."
-git add preview-1.99-book.*
+git add the_open_source_way-guidebook-2.0.*
 echo "git commit ..."
-git commit -m "updating published book to latest build from source"
+git commit -m "updating published content to latest build from source"
 # push the changes to git repo, where they are automatically deployed
 echo "git push ..."
 git push
