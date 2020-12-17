3fdba39d38283b38e2305db71188e05dbf9503b9
quaid <quaid@iquaid.org>
2020-12-16 22:42:10 -0800

    Rolling up some in-progress files
    Signed-off-by: Karsten Wade <kwade@redhat.com>

--- build-book.sh
+++ build-book.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+## echo "cleaning existing HTML and PDF ..."
+## rm the_open_source_way-guidebook-2.0.html the_open_source_way-guidebook-2.0.pdf
 echo "building html5 ..."
 asciidoctor -d book the_open_source_way-guidebook-2.0.adoc
 echo "building pdf ..."
