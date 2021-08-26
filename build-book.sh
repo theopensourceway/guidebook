2354a6c1999a1d7fd92f1dbeaaf2a0ac603cdd5c
rbowen <rbowen@rcbowen.com>
2021-08-26 11:39:42 -0400

    build epub, mobi

--- build-book.sh
+++ build-book.sh
@@ -6,6 +6,9 @@ echo "building html5 ..."
 asciidoctor -d book the_open_source_way-guidebook-2.0.adoc
 echo "building pdf ..."
 asciidoctor-pdf -d book the_open_source_way-guidebook-2.0.adoc
-echo "building epub ..."
+
+# ereader formats require asciidoctor-epub3 and kindlegen
+echo "building epub, mobi ..."
 asciidoctor-epub3 -d book the_open_source_way-guidebook-2.0.adoc
+asciidoctor-epub3 -a ebook-format=mobi -d book the_open_source_way-guidebook-2.0.adoc
 echo "done!"
